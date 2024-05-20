0f79fca4b210f4826c9df0d5f522913cb01cee2e
anshjoseph <ranshjosrph@gmail.com>
2024-05-20 14:53:44 +0530

    makenice interface for hotword and models auto loading

--- Server/Server.py
+++ Server/Server.py
@@ -1,8 +1,15 @@
 from WhisperLive import TranscriptionServer
 import argparse
+import os
 
 if __name__ == "__main__":
-    server = TranscriptionServer(use_vad=True,denoise=True,hotwords=['ansh'],model_list=["whisper_tiny_ct","tiny"])
+    
+    ASRs = [i.name for i in os.scandir("./ASR") if i.is_dir]
+    with open("./hotwords",'r') as file:
+        hotwords = [i for i in file.readlines() if (i.strip()) != 0]
+    if len(hotwords) <= 0:
+        hotwords = None
+    server = TranscriptionServer(use_vad=True,denoise=True,hotwords=hotwords,model_list=ASRs)
     parser = argparse.ArgumentParser()
     parser.add_argument('--port', '-p',
                         type=int,
